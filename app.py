bcce42d2846a3e209c2b26c54d0351c0c0c85fc8
hrqmartins <hrqmartins129@gmail.com>
2025-07-15 16:34:30 -0300

    Importando biblioteca OS, e criação da lista de restaurantes com dicionário.

---
+++ app.py
@@ -0,0 +1,5 @@
+import os
+
+restaurantes = [{'nome' : 'Sushien', 'categoria':'Japonesa', 'ativo':True}, 
+                {'nome' : 'Burger King', 'categoria':'Fast Food', 'ativo':False},
+                {'nome' : 'Pizza Hut', 'categoria':'Italiana', 'ativo':False}]  # Lista de restaurantes, cada restaurante é um dicionário
